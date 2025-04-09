3fc6301d40b6e50362c5970b26b10d50267acb7e
Makarand Parab <makarandparab@gmail.com>
2025-04-09 08:49:47 +0000

    Update base_url placeholder in api_root view

---
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = 'https://[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev/api/'
+    return Response({
+        'users': base_url + '/api/users/?format=api',
+        'teams': base_url + '/api/teams/?format=api',
+        'activities': base_url + '/api/activities/?format=api',
+        'leaderboard': base_url + '/api/leaderboard/?format=api',
+        'workouts': base_url + '/api/workouts/?format=api'
+    })
+
+# Create your views here.
